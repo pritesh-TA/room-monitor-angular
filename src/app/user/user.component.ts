cb8e226322999c4d90f095672a576b6704dc90b5
Mrunal13 <mrunal.vora13@gmail.com>
2022-06-16 16:28:19 +0530

    first-commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'rmonitor-user',
+  templateUrl: './user.component.html',
+  styleUrls: []
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
